f80e2efa2196afdf44504c61f6b376516e903a53
czachariah <zachariahchris@yahoo.com>
2020-10-12 12:12:28 -0400

    updated the test folder to show what has been completed so far and how to use it

--- test.java
+++ test.java
@@ -2,17 +2,20 @@ import java.io.File;
 
 public class test {
     public static void main(String[] args) {
+        // buliding new grid (start + end + centers + highways + blocked + unblocked)
         Grid test = new Grid();
         test.generateEntireGrid();
-        //test.printHardCenters();
-        //test.printGrid();
+        test.printHardCenters();
+        test.printGrid();
 
+        // saving a grid (that has been fully built with everything) into a file in the current directory
         File file = new File("./testGridSave.txt");
         test.saveGrid(file);
 
-        Grid test2 = new Grid();
-        test2.importGrid(file);
-        test2.printHardCenters();
+        // importing a new Grid from a file that has all the Grid contents
+        Grid test2 = new Grid();    // new Grid (will be empty)
+        test2.importGrid(file);     // file that holds other Grid contents to copy over
+        test2.printHardCenters(); 
         test2.printGrid();
     }    
 }
